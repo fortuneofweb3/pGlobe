34688d152fb2d4b1ce36457a311d069d9e7a81d8
Fortune Eze <fortuneze0@gmail.com>
2026-01-30 12:54:04 +0100

    chore: remove auxiliary scripts, external repos, and sensitive files from git index

--- scripts/debug-log.ts
+++
@@ -1,9 +0,0 @@
-console.log('START');
-import { Connection, PublicKey } from '@solana/web3.js';
-async function run() {
-    console.log('RUNNING');
-    const conn = new Connection('https://api.devnet.xandeum.com:8899');
-    const acc = await conn.getAccountInfo(new PublicKey('GHTUesiECzPRHTShmBGt9LiaA89T8VAzw8ZWNE6EvZRs'));
-    console.log('INDEX LOADED:', acc?.data.length);
-}
-run();
